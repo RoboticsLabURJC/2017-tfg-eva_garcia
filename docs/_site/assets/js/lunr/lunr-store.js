6d36ac9f8c9047b5de307811eafecdb7f8050b98
Eva Garcia Domingo <eva.garcia.domingo@avanade.com>
2021-12-19 19:37:12 +0100

    posts

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "First Week",
+        "excerpt":"First task was simple: update the repository and upload all videos with the Pi MBbot to the original JDeRobot Kids youtube repository to my personal account.  ","categories": ["Videos","December"],
+        "tags": ["week 0"],
+        "url": "/videos/december/First/",
+        "teaser": null
+      }]
