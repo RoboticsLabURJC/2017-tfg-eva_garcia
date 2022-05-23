ec9eebc319b1e3979b216bcfd9b25d2bcd8d9c90
Eva Garcia Domingo <eva.garcia.domingo@avanade.com>
2022-05-23 10:35:01 +0200

    paquete funciones python

---
+++ Modo zombie/Programa PC - python/Mbot.py
@@ -0,0 +1,88 @@
+import sys
+import serial
+from time import sleep
+
+Buzzer_Dictionary = {'B0':31,'C1':33,'D1':37,'E1':41,'F1':44,'G1':49,'A1':55,'B1':62,
+                    'C2':65,'D2':73,'E2':82,'F2':87,'G2':98,'A2':110,'B2':123,
+                    'C3':131,'D3':147,'E3':165,'F3':175,'G3':196,'A3':220,'B3':247,
+                    'C4':262,'D4':294,'E4':330,'F4':349,'G4':392,'A4':440,'B4':494,
+                    'C5':523,'D5':587,'E5':659,'F5':698,'G5':784,'A5':880,'B5':988,
+                    'C6':1047,'D6':1175,'E6':1319,'F6':1397,'G6':1568,'A6':1760,'B6':1976,
+                    'C7':2093,'D7':2349,'E7':2637,'F7':2794,'G7':3136,'A7':3520,'B7':3951,'C8':4186,'D8':4699}
+
+
+def open_PortSerial (baudios, port, timeOutSec):
+    try:
+        serial = serial.Serial(port, baudios, timeout=timeOutSec)
+        serial.setDTR(False)
+        sleep(1)
+        serial.flushInput()
+        serial.setDTR(True)
+        return serial
+    except serial.SerialException:
+        #-- Error al abrir el puerto serie
+        sys.stderr.write("Error al abrir puerto (%s)\n")
+        sys.exit(1)
+
+# funciones de serial
+def send_Message (message, serial):
+    serial.write(bytes(message, 'utf-8'))
+
+
+def read_Message (serial):
+    Data = serial.read()
+    sleep (1)
+    dataLeft = serial.inWaiting()
+    Data += serial.read(dataLeft)
+    decoded = Data.decode('utf-8')
+    return decoded
+
+# funciones de actuadores
+
+def create_Message_Motors (serial): 
+    print ("Type 'quit' for stopping or anything to send new message to motors")
+    cadena=input()
+    if (cadena.lower() == 'quit'):
+        send_Message(cadena,serial)
+        return -1
+    print ("Enter velocity for left motor (-255,255)")
+    izdo=input()
+    print ("Enter velocity for rigth motor (-255,255)")
+    dcho=input()
+    mensaje = f"{izdo};{dcho}"
+    return mensaje
+
+def create_Message_Buzzer(serial):
+    print ("Type 'quit' for stopping, 'show' for seeing all possible notes or anything to send new message to the buzzer")
+    cadena=input()
+    if (cadena.lower() == 'quit'):
+        send_Message(cadena,serial)
+        return -1
+    elif (cadena.lower() == 'show'):
+        print (Buzzer_Dictionary.keys())
+    print ("Enter the note")
+    note=input()
+    noteInt = Buzzer_Dictionary[note.upper()]
+    print ("Enter the duration -in seconds- for the note")
+    duration=input()
+    mensaje = f"{noteInt};{duration}"
+    return mensaje
+
+def create_Message_Led (serial):
+    print ("Type 'quit' for stopping or anything to send new message to leds")
+    cadena=input()
+    if (cadena.lower() == 'quit'):
+        send_Message(cadena,serial)
+        return -1
+    print ("Enter which ones led want to turn on: 0-both, 1-rigth, 2-left")
+    leds=input()
+    print ("Enter red value")
+    red=input()
+    print("Enter green value")
+    green=input()
+    print("Enter blue value")
+    blue=input()
+    mensaje = f"{leds};{red};{green};{blue}"
+    return mensaje
+
+
